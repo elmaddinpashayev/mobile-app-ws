ed81be5051f20badbe26da0a8c2e932b21eb797f
elmaddin0 <elmeddin_pasayev@mail.ru>
2022-11-02 15:04:48 +0400

    mb

--- src/main/java/com/forbrightfuture/app/ws/security/WebSecurity.java
+++ src/main/java/com/forbrightfuture/app/ws/security/WebSecurity.java
@@ -18,7 +18,6 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         this.userDetailsService = userDetailsService;
         this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
